4770e6385b61f79a94af90dd64e2f5d8f146906a
Jacky Liang <jackyliang@live.ca>
2023-08-15 17:56:24 -0400

    Configure initial project

---
+++ src/app/home/Home.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  text-align: center;
+`;
+
+function Home() {
+  return <Title>Hello world!</Title>;
+}
+
+export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+
+import "src/index.css";
+import Home from "src/app/home/Home";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
