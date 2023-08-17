03fc8600f1269b747c784fdd067890ef3e1feb87
Jacky Liang <jackyliang@live.ca>
2023-08-16 22:29:28 -0400

    Add import linting and PWA scaffolding

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,10 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
     "plugin:react-hooks/recommended",
+
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
@@ -19,11 +22,49 @@ module.exports = {
   },
   plugins: ["react-refresh"],
   rules: {
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "import/default": "off",
+    "import/no-named-as-default-member": "off",
+    "import/no-unresolved": "error",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["sibling", "parent"],
+          "index",
+          "unknown",
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
-    "@typescript-eslint/no-non-null-assertion": "off",
     semi: ["error", "always"],
+    "sort-imports": [
+      "error",
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true, // don"t want to sort import lines, use eslint-plugin-import instead
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+        allowSeparatedGroups: true,
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project: "./tsconfig.json",
+      },
+    },
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
@@ -7,5 +8,51 @@ export default defineConfig({
   plugins: [
     react({ plugins: [["@swc/plugin-styled-components", {}]] }),
     tsconfigPaths(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: [
+        "favicon.ico",
+        "favicon.svg",
+        "apple-touch-icon-180x180.png",
+      ],
+      manifest: {
+        name: "Huebama",
+        short_name: "Huebama",
+        description: "Huebama's personal website",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      registerType: "autoUpdate",
+    }),
   ],
 });
