5d22ae57f3ff117102ad36ba2a69b5b70a53299e
Jacky Liang <jackyliang@live.ca>
2023-09-02 17:29:01 -0400

    Use page structure to organise files

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "src/index.css";
-import Home from "src/app/home/Home";
+import Home from "src/app/pages/Home";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
