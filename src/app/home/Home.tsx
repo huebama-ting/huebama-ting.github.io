cc578a4cbcb22818ece7fb669e3e15819445c14d
Jacky Liang <jackyliang@live.ca>
2023-09-02 16:39:09 -0400

    Add home icon

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -1,11 +1,38 @@
 import { styled } from "styled-components";
 
+import homeIconPng from "src/app/assets/home_icon.png";
+import homeIconWebp from "src/app/assets/home_icon.webp";
+
+const Container = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  align-items: center;
+`;
+const Image = styled.img`
+  border-radius: 50%;
+  max-width: 10rem;
+  max-height: 10rem;
+`;
 const Title = styled.h1`
   text-align: center;
 `;
 
 function Home() {
-  return <Title>Hello world!</Title>;
+  return (
+    <Container>
+      <picture>
+        <source type="image/webp" srcSet={homeIconWebp} />
+        <source type="image/png" srcSet={homeIconPng} />
+        <Image
+          src={homeIconPng}
+          width="1000"
+          height="1000"
+          alt="Icon showing a lazy egg"
+        />
+      </picture>
+      <Title>Hello world!</Title>
+    </Container>
+  );
 }
 
 export default Home;
