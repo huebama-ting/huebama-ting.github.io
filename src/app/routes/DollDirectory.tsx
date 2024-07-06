654db075156cc348647c08409e26e17b960fb321
Jacky Liang <jackyliang@live.ca>
2024-07-06 04:43:10 -0400

    Lazy load component

--- src/app/routes/DollDirectory.tsx
+++ src/app/routes/DollDirectory.tsx
@@ -1,9 +1,12 @@
 import Sheet from "@mui/joy/Sheet";
-import { useEffect, useState } from "react";
+import { lazy, useEffect, useState } from "react";
 
-import { DollCard } from "src/app/components/doll/DollCard";
 import { Container } from "src/app/shared/Layout";
 import { Doll } from "src/app/types/doll";
+
+const DollCard = lazy(
+  () => import("src/app/components/doll/DollCard"),
+);
 
 export function DollDirectory() {
   const [dolls, setDolls] = useState<Doll[]>([]);
