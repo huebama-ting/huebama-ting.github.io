9e83a1037b4dc527370736cdaaa73ad7c8b3a312
Jacky Liang <jackyliang@live.ca>
2024-07-06 17:39:12 -0400

    Darken doll card image at bottom and add skeleton

--- src/app/components/doll/DollCard.tsx
+++ src/app/components/doll/DollCard.tsx
@@ -1,8 +1,11 @@
 import styled from "@emotion/styled";
+import Box from "@mui/joy/Box";
 import Card from "@mui/joy/Card";
 import CardContent from "@mui/joy/CardContent";
 import CardCover from "@mui/joy/CardCover";
+import Skeleton from "@mui/joy/Skeleton";
 import Typography from "@mui/joy/Typography";
+import { useState } from "react";
 
 import {
   CDN_BASE_URL,
@@ -21,28 +24,51 @@ const Image = styled.img`
 
 export function DollCard(props: DollCardProps) {
   const imageUrl = `${CDN_BASE_URL}/${DOLL_INFO_REPO_FILES_PATH}/${props.doll.cardImageUrl}`;
+  const [loading, setLoading] = useState<boolean>(true);
 
   return (
     <Card sx={{ height: "16rem", width: "8rem" }}>
       <CardCover>
-        <picture>
-          <source
-            type="image/webp"
-            width="256"
-            height="512"
-            srcSet={`${imageUrl}.webp`}
-          />
-          <Image
-            src={`${imageUrl}.png`}
-            width="256"
-            height="512"
-            alt="Icon showing a lazy egg"
-            loading="lazy"
-          />
-        </picture>
+        <Skeleton loading={loading}>
+          <picture
+            onLoad={() => {
+              setLoading(false);
+            }}
+          >
+            <source
+              type="image/webp"
+              width="256"
+              height="512"
+              srcSet={`${imageUrl}.webp`}
+            />
+            <Image
+              src={`${imageUrl}.png`}
+              width="256"
+              height="512"
+              alt="Icon showing a lazy egg"
+              loading="lazy"
+            />
+          </picture>
+          <Box
+            sx={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              background:
+                "linear-gradient(to bottom, rgba(0,0,0,0) 60%, rgba(0,0,0,1))",
+            }}
+          ></Box>
+        </Skeleton>
       </CardCover>
       <CardContent>
-        <Typography level="body-lg" fontWeight="lg" mt="14.5rem">
+        <Typography
+          level="body-lg"
+          fontWeight="lg"
+          mt="14.5rem"
+          textColor="#bbb"
+        >
           {props.doll.nameEn}
         </Typography>
       </CardContent>
