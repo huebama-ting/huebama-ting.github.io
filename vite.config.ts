0ba6769424ccf455d26b83e8448da9c967d85d3d
Jacky Liang <jackyliang@live.ca>
2024-07-07 02:12:04 -0400

    Specify ES2022 Vite build target

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    target: "es2022",
     cssMinify: "lightningcss",
   },
   css: {
