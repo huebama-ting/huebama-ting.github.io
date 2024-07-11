436270f7433b2e9ffd266fced4c55b7ec657b0fc
Jacky Liang <jackyliang@live.ca>
2024-07-10 22:45:50 -0400

    Update ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,62 +8,90 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-
-    "plugin:@typescript-eslint/strict-type-checked",
-    "plugin:@typescript-eslint/stylistic",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
+    "plugin:@eslint-react/recommended-legacy",
+    "plugin:compat/recommended",
+    "plugin:depend/recommended",
+    "plugin:es-x/restrict-to-es2022",
+    "plugin:import-x/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:n/recommended",
+    "plugin:promise/recommended",
     "plugin:react-hooks/recommended",
-
+    "plugin:sonarjs/recommended-legacy",
     "prettier",
   ],
-  parser: "@typescript-eslint/parser",
+  overrides: [
+    {
+      extends: ["plugin:jsonc/prettier", "plugin:jsonc/recommended-with-json"],
+      files: ["*.json", "*.json5", "*.jsonc"],
+      parser: "jsonc-eslint-parser",
+    },
+    {
+      extends: [
+        "plugin:@typescript-eslint/strict-type-checked",
+        "plugin:@typescript-eslint/stylistic",
+        "plugin:import-x/typescript",
+      ],
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: [
+          "./tsconfig.json",
+          "./tsconfig.node.json",
+          "./tsconfig.eslint.json",
+        ],
+        tsconfigRootDir: __dirname,
+      },
+      rules: {
+        "@eslint-react/no-leaked-conditional-rendering": "error",
+        "@eslint-react/prefer-read-only-props": "error",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          {
+            args: "all",
+            argsIgnorePattern: "^_",
+            caughtErrors: "all",
+            caughtErrorsIgnorePattern: "^_",
+            destructuredArrayIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+            ignoreRestSiblings: true,
+          },
+        ],
+        "@typescript-eslint/restrict-template-expressions": [
+          "error",
+          {
+            allowBoolean: true,
+            allowNumber: true,
+          },
+        ],
+      },
+    },
+  ],
   parserOptions: {
-    ecmaVersion: "2022",
     ecmaFeatures: {
       jsx: true,
     },
+    ecmaVersion: 2022,
     sourceType: "module",
-    project: [
-      "./tsconfig.json",
-      "./tsconfig.node.json",
-      "./tsconfig.eslint.json",
-    ],
-    tsconfigRootDir: __dirname,
   },
-  plugins: ["@emotion", "react-refresh"],
+  plugins: [
+    "@emotion",
+    "compat",
+    "es-x",
+    "jsx-a11y",
+    "promise",
+    "react-refresh",
+    "sonarjs",
+  ],
   rules: {
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        args: "all",
-        argsIgnorePattern: "^_",
-        caughtErrors: "all",
-        caughtErrorsIgnorePattern: "^_",
-        destructuredArrayIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-        ignoreRestSiblings: true,
-      },
-    ],
-    "@typescript-eslint/restrict-template-expressions": [
-      "error",
-      {
-        allowBoolean: true,
-        allowNumber: true,
-      },
-    ],
-    "import/default": "off",
-    "import/no-named-as-default-member": "off",
-    "import/no-unresolved": [
+    "import-x/no-unresolved": [
       "error",
       {
         ignore: ["^virtual:"],
       },
     ],
-    "import/order": [
+    "import-x/order": [
       "error",
       {
         groups: [
@@ -81,16 +109,23 @@ module.exports = {
         },
       },
     ],
+    "n/no-missing-import": "off",
+    "n/no-unsupported-features/node-builtins": [
+      "error",
+      {
+        ignores: ["navigator"],
+      },
+    ],
+    "n/prefer-node-protocol": "error",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
-    semi: ["error", "always"],
     "sort-imports": [
       "error",
       {
         ignoreCase: false,
-        ignoreDeclarationSort: true, // don't want to sort import lines, use eslint-plugin-import instead
+        ignoreDeclarationSort: true, // don't want to sort import lines, use eslint-plugin-import-x instead
         ignoreMemberSort: false,
         memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
         allowSeparatedGroups: true,
@@ -98,13 +133,9 @@ module.exports = {
     ],
   },
   settings: {
-    "import/resolver": {
-      typescript: {
-        project: "./tsconfig.json",
-      },
-    },
-    react: {
-      version: "detect",
+    "import-x/resolver": {
+      node: true,
+      typescript: true,
     },
   },
   reportUnusedDisableDirectives: true,
