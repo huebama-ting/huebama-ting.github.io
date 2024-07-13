97577e41d457e82d378e792a7259949c0d54be83
Jacky Liang <jackyliang@live.ca>
2024-07-13 02:16:59 -0400

    Add additional Vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { resolve } from "node:path";
 
 import eslintPlugin from "@nabla/vite-plugin-eslint";
 import react from "@vitejs/plugin-react-swc";
+import TurboConsole from "unplugin-turbo-console/vite";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import { qrcode } from "vite-plugin-qrcode";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
@@ -30,11 +32,13 @@ export default defineConfig({
   },
   plugins: [
     eslintPlugin(),
+    qrcode(),
     react({
       devTarget: "es2022",
       plugins: [["@swc/plugin-emotion", {}]],
     }),
     tsconfigPaths(),
+    TurboConsole(),
     VitePWA({
       devOptions: {
         enabled: true,
