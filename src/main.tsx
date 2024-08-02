9a8d5028273de77cbac3d9f3fd0fa593c421e768
Jacky Liang <jackyliang@live.ca>
2024-08-02 02:16:24 -0400

    Tweak main

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "@mantine/core/styles.css";
 
 import { MantineProvider } from "@mantine/core";
 import { StrictMode } from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import "src/index.css";
@@ -36,7 +36,6 @@ const router = createBrowserRouter([
 
           return { Component: Home };
         },
-        errorElement: <ErrorElement />,
       },
       {
         path: Routes.COOP_REPORT,
@@ -72,7 +71,7 @@ const router = createBrowserRouter([
 ]);
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider>
       <RouterProvider router={router} />
