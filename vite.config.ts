bcd772a0dff17d7415ff9eb96d274d6196d7c9a6
Jacky Liang <jackyliang@live.ca>
2024-08-01 22:50:50 -0400

    Add Mantine

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    cssMinify: "lightningcss",
     target: "es2022",
     rollupOptions: {
       output: {
