b04e35a175a24683964a6a6e9aa3c12b48e9516b
Jacky Liang <jackyliang@live.ca>
2024-08-11 21:18:05 -0400

    Use auto colour scheme

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import "@glidejs/glide/dist/css/glide.theme.min.css";
 import "@mantine/carousel/styles.css";
 import "@mantine/core/styles.css";
 
-import { MantineProvider } from "@mantine/core";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
@@ -73,7 +73,8 @@ const router = createBrowserRouter([
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MantineProvider>
+    <ColorSchemeScript defaultColorScheme="auto" />
+    <MantineProvider defaultColorScheme="auto">
       <RouterProvider router={router} />
       <PwaReloadPrompt />
     </MantineProvider>
