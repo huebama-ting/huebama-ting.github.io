b302a6de47eb542b0274e70e29ed9e33e5fe0688
Jacky Liang <jackyliang@live.ca>
2024-08-12 18:43:29 -0400

    Update Stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,13 +2,5 @@
 
 export default {
   extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
-  customSyntax: "postcss-styled-syntax",
-  rules: {
-    "font-family-no-missing-generic-family-keyword": [
-      true,
-      {
-        ignoreFontFamilies: ["Overpass Variable"],
-      },
-    ],
-  },
+  ignoreFiles: ["dist/**/*"],
 };
