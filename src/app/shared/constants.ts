817eca2d170e29c97742dd4f380791b222152726
Jacky Liang <jackyliang@live.ca>
2024-08-14 21:29:52 -0400

    Remove redundant constants

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -5,19 +5,5 @@ export const enum Routes {
   DOLL_INFO = "dolls/:name",
 }
 
-const BREAKPOINTS: Record<string, number> = {
-  sm: 640,
-  md: 768,
-  lg: 1024,
-  xl: 1280,
-};
-
-export const MEDIA_QUERIES = Object.keys(BREAKPOINTS)
-  .map((key) => [key, BREAKPOINTS[key]] as [string, number])
-  .reduce<Record<string, string>>((prev, [key, breakpoint]) => {
-    prev[key] = `@media (min-width: ${breakpoint}px)`;
-    return prev;
-  }, {});
-
 export const CDN_BASE_URL = "https://cdn.jsdelivr.net/gh";
 export const DOLL_INFO_REPO_IMAGES_PATH = "huebama-ting/doll-info/images";
