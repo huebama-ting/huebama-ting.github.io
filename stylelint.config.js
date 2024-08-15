bde342b6b151d42cbaf8074cd46f8e399e7a0928
Jacky Liang <jackyliang@live.ca>
2024-08-14 21:30:15 -0400

    Update Stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,6 +4,7 @@ export default {
   extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
   ignoreFiles: ["dist/**/*"],
   rules: {
+    "at-rule-no-unknown": [true, { ignoreAtRules: ["mixin"] }],
     "media-query-no-invalid": null,
   },
 };
